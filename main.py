84ee26116f6fad8ccb46a09aa0ab8c38ea5dc7e3
Alec Meyer <alecrmeyer4@gmail.com>
2025-07-11 15:12:32 -0600

    first commit for this repo

---
+++ main.py
@@ -0,0 +1,59 @@
+
+from app import app
+
+
+
+from flask import Flask, request, render_template
+
+import stores.boarshat 
+import stores.nrg
+import stores.pastimes
+# Flask constructor
+app = Flask(__name__, static_url_path='/static', static_folder='static' )
+  
+nrg = stores.nrg.NRG()
+pt = stores.pastimes.Pastimes()
+bh = stores.boarshat.BoarsHat()
+
+nrg_storage = {
+   "id": "nrg",
+   "Name": "Nerd Rage Gaming",
+   "obj": nrg,
+}
+pt_storage = {
+   "id": "pt",
+   "Name": "Pastimes",
+   "obj": pt,
+}
+bh_storage = {
+   "id": "bh",
+   "Name": "Boars Hat",
+   "obj": bh,
+}
+
+store_list = [nrg_storage, pt_storage, bh_storage]
+
+@app.route('/', methods =["GET", "POST"])
+def cardstores():
+
+   if request.method == "POST":
+      chosen_stores = request.form.getlist("cardstore")
+      card = request.form.get("card")
+
+      for store in store_list:
+         if store["id"] in chosen_stores:
+            store["quant"] = store["obj"].get_card(card)
+         else:
+            store["quant"] = -1 
+
+         
+      return render_template("output.html", store_list=store_list, card=card)    
+   return render_template("form.html")
+
+if __name__=='__main__':
+   app.run(debug=True, use_reloader=True)
+
+
+
+
+
---
+++ stores/boarshat.py
@@ -0,0 +1,36 @@
+from bs4 import BeautifulSoup
+import requests
+
+class BoarsHat:
+    url_base = 'https://www.boarshatgaming.com/products/search?q='
+
+    # Convert card text to the url 
+    def query_to_url(self, url, query):
+        ret = url + query.replace(" ", "+")
+        return ret
+
+    # return X from the phrase "X In Stock"
+    def get_quant_from_text(self, text):
+        split_text = text.split()
+        if split_text[0] == "Out":
+            return 0
+        else: 
+            return split_text[0]
+
+    def get_card(self, card_name):
+        total_count = 0
+        url = self.query_to_url(self.url_base, card_name)
+        response = requests.get(url)
+        html_content = response.content
+        soup = BeautifulSoup(html_content, 'html5lib')
+        products = soup.find('div', class_='products-container browse')
+        all_items = products.find_all('div', class_='inner')
+        for card in all_items:
+            name = card.find('h4', class_='name small-12 medium-4')
+            if name != None and (name.text == card_name or (card_name + " -") in name.text):
+                variants = card.find('div', class_='variants')
+                quantities = variants.find('span', class_ = 'variant-short-info variant-qty')
+                for quantity in quantities:
+                    count = self.get_quant_from_text(quantity.text.strip())
+                    total_count += int(count)
+        return total_count
